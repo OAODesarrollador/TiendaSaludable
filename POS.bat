c1a325cf403d08966ac14cf37c88d4660faf4888
oaodesarrollador <oaodesarrollador@gmail.com>
2025-10-18 19:58:19 -0300

    POS.bat Corregido

--- POS.bat
+++ POS.bat
@@ -1,4 +1,15 @@
 @echo off
+:: --- Forzar ejecución como administrador ---
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Solicitando permisos de administrador...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
+
+
+@echo off
 title Iniciando Proyecto POS
 color 0A
 
